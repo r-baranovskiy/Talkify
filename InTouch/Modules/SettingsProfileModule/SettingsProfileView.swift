8324aa81fb2022fbedd12d8d6d77de8b7769a9e8
koko6a <rus.baranovskiy@gmail.com>
2023-02-17 23:00:26 +0300

    Refactoring SettingsViewController

---
+++ InTouch/Modules/SettingsProfileModule/SettingsProfileView.swift
@@ -0,0 +1,161 @@
+import UIKit
+
+protocol SettingsProfileViewDelegate: AnyObject {
+    func backButtonDidTap()
+    func goToChatButtonDidTap()
+}
+
+final class SettingsProfileView: UIView {
+    
+    weak var delegate: SettingsProfileViewDelegate?
+    
+    //MARK: - UI Constants
+    
+    //Labels
+    private let welcomeLabel = UILabel(
+        text: "Ваш профиль", font: .avenir40(),
+        adjustsFontSizeToFitWidth: true, alignment: .center)
+    
+    private let nameLabel = UILabel(text: "Имя (никнейм)", font: .avenir20())
+    private let aboutInfoLabel = UILabel(text: "О себе:", font: .avenir20())
+    private let sexLabel = UILabel(text: "Ваш пол:", font: .avenir20())
+    
+    //Buttons
+    private let backButton = CustomButton.createBackButton()
+    private let goToChatButton = CustomButton(
+        text: "Общаться!", isShadow: true)
+    
+    //TextFields
+    private (set) var nameTextField = CustomTextField.oneLineTextField()
+    private (set) var aboutInfoTextField = CustomTextField.oneLineTextField()
+    
+    private (set) var userImageView: UIImageView = {
+        let imageView = UIImageView(
+            imageName: KeysImages.emptyPhoto.rawValue,
+            contentMode: .scaleAspectFit, addBorder: true)
+        imageView.isUserInteractionEnabled = true
+        return imageView
+    }()
+    
+    private let sexSegmentedControl = UISegmentedControl(items: ["Мужской", "Женский"])
+    
+    //Containers
+    private var nameStackView = UIStackView()
+    private var aboutInfoStackView = UIStackView()
+    private var sexStackView = UIStackView()
+    
+    //MARK: - Init
+    
+    override init(frame: CGRect) {
+        super .init(frame: frame)
+        backgroundColor = .backgoundImage()
+        sexSegmentedControl.selectedSegmentIndex = 0
+        configureStackViews()
+        setTargets()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        userImageView.layer.cornerRadius = userImageView.frame.height / 2
+    }
+    
+    // MARK: - Behaviour
+    
+    private func setTargets() {
+        backButton.addTarget(self, action: #selector(backButtonTapped),
+                             for: .touchUpInside)
+        goToChatButton.addTarget(self, action: #selector(goToChatButtonTapped),
+                                 for: .touchUpInside)
+    }
+    
+    @objc private func backButtonTapped() {
+        delegate?.backButtonDidTap()
+    }
+    
+    @objc private func goToChatButtonTapped() {
+        delegate?.goToChatButtonDidTap()
+    }
+    
+    //MARK: - Configue StackViews
+    
+    private func configureStackViews() {
+        nameStackView = UIStackView(
+            arrangedSubviews: [nameLabel, nameTextField],
+            axis: .vertical, spacing: 5, alignment: .fill)
+        
+        aboutInfoStackView = UIStackView(
+            arrangedSubviews: [aboutInfoLabel, aboutInfoTextField],
+            axis: .vertical, spacing: 5, alignment: .fill)
+        
+        sexStackView = UIStackView(
+            arrangedSubviews: [sexLabel, sexSegmentedControl],
+            axis: .vertical, spacing: 10, alignment: .fill)
+    }
+    
+    //MARK: - Constraints
+    
+    private func setConstraints() {
+        addSubviewWithoutTranslates(
+            welcomeLabel, backButton, userImageView, nameStackView,
+            aboutInfoStackView, sexStackView, goToChatButton)
+        
+        NSLayoutConstraint.activate([
+            backButton.topAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.topAnchor, constant: 16),
+            backButton.leadingAnchor.constraint(
+                equalTo: leadingAnchor, constant: 16),
+            backButton.heightAnchor.constraint(equalToConstant: 30),
+            
+            welcomeLabel.topAnchor.constraint(
+                greaterThanOrEqualTo: backButton.bottomAnchor, constant: 20),
+            welcomeLabel.leadingAnchor.constraint(
+                equalTo: leadingAnchor, constant: 20),
+            welcomeLabel.trailingAnchor.constraint(
+                equalTo: trailingAnchor, constant: -20),
+            
+            userImageView.topAnchor.constraint(
+                equalTo: welcomeLabel.bottomAnchor, constant: 20),
+            userImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            userImageView.widthAnchor.constraint(
+                equalTo: widthAnchor, multiplier: 0.35),
+            userImageView.heightAnchor.constraint(
+                equalTo: userImageView.widthAnchor),
+            
+            nameStackView.topAnchor.constraint(
+                lessThanOrEqualTo: userImageView.bottomAnchor, constant: 50),
+            nameStackView.leadingAnchor.constraint(
+                equalTo: leadingAnchor, constant: 20),
+            nameStackView.trailingAnchor.constraint(
+                equalTo: trailingAnchor, constant: -20),
+            
+            aboutInfoStackView.topAnchor.constraint(
+                lessThanOrEqualTo: nameStackView.bottomAnchor, constant: 50),
+            aboutInfoStackView.leadingAnchor.constraint(
+                equalTo: leadingAnchor, constant: 20),
+            aboutInfoStackView.trailingAnchor.constraint(
+                equalTo: trailingAnchor, constant: -20),
+            
+            sexStackView.topAnchor.constraint(
+                equalTo: aboutInfoStackView.bottomAnchor, constant: 20),
+            sexStackView.leadingAnchor.constraint(
+                equalTo: leadingAnchor, constant: 20),
+            sexStackView.trailingAnchor.constraint(
+                equalTo: trailingAnchor, constant: -20),
+            
+            goToChatButton.topAnchor.constraint(
+                equalTo: sexStackView.bottomAnchor, constant: 40),
+            goToChatButton.leadingAnchor.constraint(
+                equalTo: leadingAnchor, constant: 80),
+            goToChatButton.trailingAnchor.constraint(
+                equalTo: trailingAnchor, constant: -80),
+            goToChatButton.bottomAnchor.constraint(
+                greaterThanOrEqualTo: safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            goToChatButton.heightAnchor.constraint(equalToConstant: 54)
+        ])
+    }
+}
---
+++ InTouch/Modules/SettingsProfileModule/SettingsProfileViewController.swift
@@ -0,0 +1,143 @@
+import UIKit
+
+class SettingsProfileViewController: UIViewController, UINavigationControllerDelegate {
+    
+    private let settingsProfileView = SettingsProfileView()
+    
+    private var userImageView = UIImageView()
+    
+    private var nameTextField = UITextField()
+    private var aboutInfoTextField = UITextField()
+    
+    //MARK: - Lifecycles
+    
+    override func loadView() {
+        super.loadView()
+        view = settingsProfileView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configurePhotoImageView()
+        addKeyboardNotification()
+        setDelegates()
+        addKeyboardNotification()
+        hideKeyboardWhenTappedAround()
+    }
+    
+    // MARK: - Behaviour
+    
+    private func setDelegates() {
+        nameTextField = settingsProfileView.nameTextField
+        aboutInfoTextField = settingsProfileView.aboutInfoTextField
+        
+        settingsProfileView.delegate = self
+    }
+    
+    private func addKeyboardNotification() {
+        NotificationCenter.default.addObserver(
+            self, selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(
+            self, selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    //Buttons' tapp
+    @objc private func userImageViewTapped() {
+        presentPhotoActionAlert()
+    }
+    
+    private func configurePhotoImageView() {
+        userImageView = settingsProfileView.userImageView
+        let photoTapRecognizer = UITapGestureRecognizer(
+            target: self, action: #selector(userImageViewTapped))
+        userImageView.addGestureRecognizer(photoTapRecognizer)
+    }
+    
+    // MARK: - Appearance
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (
+            notification.userInfo?[
+                UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+}
+
+// MARK: - SettingsProfileViewDelegate
+
+extension SettingsProfileViewController: SettingsProfileViewDelegate {
+    func backButtonDidTap() {
+        dismiss(animated: true)
+    }
+    
+    func goToChatButtonDidTap() {
+        print("CHAT")
+    }
+}
+
+//MARK: - UIImagePickerControllerDelegate
+
+extension SettingsProfileViewController: UIImagePickerControllerDelegate {
+    
+    private func presentPhotoActionAlert() {
+        let changeAlert = UIAlertController(
+            title: "Фото для профиля",
+            message: "Хотите создать новое фото или выбрать из существующих?",
+            preferredStyle: .actionSheet)
+        
+        changeAlert.addAction(UIAlertAction(
+            title: "Сделать фото", style: .default, handler: { [weak self] _ in
+            self?.presentCamera()
+        }))
+        
+        changeAlert.addAction(UIAlertAction(
+            title: "Выбрать из галереи", style: .default, handler: { [weak self] _ in
+            self?.presentPhotoPicker()
+        }))
+        
+        changeAlert.addAction(
+            UIAlertAction(title: "Отменить", style: .cancel))
+        
+        present(changeAlert, animated: true)
+    }
+    
+    private func presentPhotoPicker() {
+        let photoPickerVC = UIImagePickerController()
+        photoPickerVC.sourceType = .photoLibrary
+        photoPickerVC.delegate = self
+        photoPickerVC.allowsEditing = true
+        present(photoPickerVC, animated: true)
+    }
+    
+    private func presentCamera() {
+        let photoPickerVC = UIImagePickerController()
+        photoPickerVC.sourceType = .camera
+        photoPickerVC.delegate = self
+        photoPickerVC.allowsEditing = true
+        present(photoPickerVC, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
+            return
+        }
+        userImageView.image = image
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
+    }
+}
